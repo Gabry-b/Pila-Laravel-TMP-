893b1400d3fd232990d60ff883cdc5db301e1d4d
Robertomdh <robertt28850@gmail.com>
2022-06-01 18:30:08 +0200

    amapola

---
+++ resources/views/elden_inspiracion.blade.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="https://kit.fontawesome.com/2a4935d0f4.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+        <!--NAV-->
+        <nav class="navbar navbar-expand-lg navbar-light" aria-label="Eleventh navbar example">
+          <div class="container-fluid">
+              <a class="navbar-brand mr-auto navhover2" href="home.html"><img src="img/logoMW.svg"></a>
+              <button class="navbar-toggler bg-light" 
+                      type="button" 
+                      data-bs-toggle="collapse" 
+                      data-bs-target="#navbarSupportedContent"
+                      aria-expanded="false"
+                      aria-label="Toogle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav ms-auto">
+                      <li class="nav-item navhover"><a class="nav-link" href="home_berserk.html"><img style="margin:15px" src="navimg/berserklogo.png" width="50px"></a></li>
+                      <li class="nav-item navhover"><a class="nav-link" href="home_esdla.html"><img style="margin:15px" src="navimg/esdlalogo.png" width="50px"></a></li>
+                      <li class="nav-item navhover"><a class="nav-link" href="home_eldenring.html"><img style="margin:15px" src="navimg/eldenringlogo.png" width="50px"></a></li>
+                  </ul>
+              </div>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                  <li class="nav-item navhover2"><a class="nav-link" href="contacta.html"><img src="navimg/contactlogo.svg" width="80px"></a></li>
+                  <li class="nav-item navhover2"><a class="nav-link" href="login.html"><img src="navimg/loginsignin_logo.svg" width="110px"></a></li>
+                  <li class="nav-item navhover2"><a class="nav-link" href="perfil.html"><img src="navimg/perfil_logo.svg" width="50px"></a></li>
+                </ul>
+              </div>
+          </div>
+      </nav>
+      <!--SEPARADOR-->
+<div id="separator">
+  <div class="content"></div>
+</div>
+    <!--NOTICIA-->
+    <article>
+        <section>
+            <div class="container-fluid py-2 text-center">
+                <h1 class="display-4">¡Juega a las obras que inspiraron a Miyazaki!</h1></br>
+                <p class="text-muted">Conoce (y juega) a los juegos retro que inspiraron a Hidetaka Miyazaki para crear sus videojuegos de la saga Souls</p></br>
+                <img src="noticias/elden/c3.jpg" class="w-50" alt="">
+                </br>
+            </div>
+            <div class="container py-2 text-center">
+                <div class="text-center  col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead" id="noticias">
+                  Siempre se habla de los juegos que han sido inspirados por la famosa fórmula "Soulslike" (que no son pocos), pero esta vez venimos a hablar de la situación inversa
+                </div>
+                <p class="col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead">
+                  Os presentamos de manera jugable los 3 títulos que inspiraron a Miyazaki para la temática de Demon Souls, Dark Souls, Bloodborne y Elden Ring
+                
+                <!--PRIMER JUEGO-->
+                <div class="col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead">
+                <h1>1. Castlevania NES</h1>
+                <br/>
+                <img src="noticias/elden/c3.1.jpg" class="w-50" alt="">
+                <br/><br/>
+                <p><b>Nombre:</b> Castlevania</p>
+                <p><b>Año:</b> 1986</p>
+                <p><b>Desarrollador: </b> Konami</p>
+                <p><b>Argumento:</b> Es el año 1691. La tierra de Transilvania ha permanecido en paz por más de cien años gracias a los esfuerzos de Christopher Belmont. Los campesinos y aldeanos han empezado a purgar sus mentes de los recuerdos de aquellos tiempos en que sus tierras fueron dominadas por caos y sombras, tiempos en que los no muertos caminaban sobre la tierra. Sin embargo, otros recordaron que cada cien años el Conde Drácula regresa a plagar la tierra, trayendo consigo las fuerzas del infierno. Así, una noche, el Príncipe de la Oscuridad se levantó y retornó a Castlevania, su hogar ancestral, llamando a sus demonios para purgar el mundo de los humanos.
+                  La gente clamó por un héroe, alguien que los defendiera de los malignos deseos del conde.</p>
+                <p><b>Similitudes con la saga Souls:</b> Ambiente oscuro y opresivo cargado de criaturas (solo hay que verlo)</p>
+                <p><iframe src="https://www.retrogames.cc/embed/20903-castlevania-usa.html" width="600" height="450" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe></p>
+                <p><b>NOTA: para eliminar el anuncio, dale al play y aparecerá una cruz en la esquina superior derecha ;)</b></p>
+                <p><b>Controles (configurables en la parte superior derecha)</b></p>
+                <p><b>Enter:</b> Start</p>
+                <p><b>← ↑ →</b> Moverse</p>
+                <p><b>A:</b> Z</p>
+                <p><b>B:</b> X</p>
+                <p><b>X:</b> A</p>
+                <p><b>Y:</b> S</p>
+                </div>
+              
+                <!--SEGUNDO JUEGO-->
+                <div class="col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead">
+                  <h1>2. Ghouls'n Ghosts</h1>
+                  <br/>
+                  <img src="noticias/elden/c3.2.jpg" class="w-50" alt="">
+                  <br/><br/>
+                  <p><b>Nombre:</b> Ghouls'n Ghosts</p>
+                  <p><b>Año:</b> 1988</p>
+                  <p><b>Desarrollador: </b> Capcom</p>
+                  <p><b>Similitudes con la saga Souls:</b> La temática medieval, donde eres un caballero solitario con una misión y objetivo principal que deberás de cumplir superando obstáculos tales como criaturas y bestias demoníacas</p>
+                  <p><iframe src="https://www.retrogames.cc/embed/8661-ghoulsn-ghosts-us.html" width="600" height="450" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe></p>
+                  <p><b>NOTA: para eliminar el anuncio, dale al play y aparecerá una cruz en la esquina superior derecha ;)</b></p>
+                  <p><b>Controles (configurables en la parte superior derecha)</b></p>
+                  <p><b>Enter:</b> Start</p>
+                  <p><b>← ↑ →</b> Moverse</p>
+                  <p><b>A:</b> Z</p>
+                  <p><b>B:</b> X</p>
+                  <p><b>X:</b> A</p>
+                  <p><b>Y:</b> S</p>
+                  </div>
+
+                  <!--TERCER JUEGO-->
+                <div class="col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead">
+                  <h1>3. Chakan, The Forever Man</h1>
+                  <br/>
+                  <img src="noticias/elden/c3.3.jpg" class="w-50" alt="">
+                  <br/><br/>
+                  <p><b>Nombre:</b> Chakan, The Forever Man</p>
+                  <p><b>Año:</b> 1992</p>
+                  <p><b>Desarrollador: </b> Extended Play Records</p>
+                  <p><b>Argumento:</b> El videojuego sigue la historia de Chakan, un guerrero quien estaba tan confiado de sus habilidades marciales (especialmente su habilidad con la espada sin igual) y habilidades talentosas en la hechicería y la alquimia, que declaró que ni siquiera la mismísima Muerte podría superarlo en una batalla. Por supuesto, la Muerte se hizo presente y desafió a Chakan con una proposición. Si Chakan podía vencerle, le concedería la vida eterna. Sin embargo, si Chakan era derrotado, se convertiría en el sirviente eterno de la Muerte. Aunque esa es la premisa que se anuncia en el videojuego, en otro historia paralela, Chakan era tan temido por los ancianos de varias tribus, que dejaron a un lado sus pequeñas diferencias y amasaron como encontrar una manera de derrotarlo o contenerlo, y al escuchar sobre su alarde, convocaron a la Parca ellos mismos, informando al Segador de sus fanfarronadas, y lo contrataron para derrotar a Chakan.</p>
+                  <p><b>Similitudes con la saga Souls:</b> Algo más similar al ambiente Bloodborne, comparte la jugabilidad zona-jefe y esa temática basada en el terror cósmico</p>
+                  <p><iframe src="https://www.retrogames.cc/embed/30305-chakan-usa-europe.html" width="600" height="450" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe></p>
+                  <p><b>NOTA: para eliminar el anuncio, dale al play y aparecerá una cruz en la esquina superior derecha ;)</b></p>
+                  <p><b>Controles (configurables en la parte superior derecha)</b></p>
+                  <p><b>Enter:</b> Start</p>
+                  <p><b>← ↑ →</b> Moverse</p>
+                  <p><b>A:</b> Z</p>
+                  <p><b>B:</b> X</p>
+                  <p><b>X:</b> A</p>
+                  <p><b>Y:</b> S</p>
+                  </div>
+                  <div class="text-center  col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead" id="noticias">
+                    Y hasta aquí nuestra sección de videojuegos que inspiraron al gran Miyazaki, esperamos desde Medieval World que disfrutéis de ellos ;)
+                  </div>
+            </div>
+        </section>
+    </article>
+<!--PUBLICADO POR-->
+<div class="row align-items-center mb-4">
+  <div class="col-lg-3"></div>
+  <div class="col-md-6 col-lg-3 col-sm-6 text-center text-lg-left mb-3 m-lg-0">
+    <img
+      src="img/micareto.png"
+      alt="Avatar"
+      class="rounded shadow-1-strong mr-2"
+      height="35"/>
+    <span>Publicado <u>15.05.2022</u> por</span>
+    <a href="#" class="text-dark">José Sanchís AKA René Sanchís</a>
+  </div>
+  <div class="col-lg-3 col-md-6 col-sm-6 text-center text-lg-center">
+    <!-- Twitter -->
+    <a
+      class="btn btn-primary px-3 mr-1"
+      style="background-color: #55acee;"
+      href="https://twitter.com/bloodyReneTV"
+      role="button"
+      ><i class="fab fa-twitter"></i></a>
+  </div>
+</div>
+<!--SEPARADOR-->
+<div id="separator">
+  <div class="content"></div>
+</div>
+<!--NEWSLETTER-->
+<!--Footer-->
+<section class="prueba" style="background-image: url('IMAGES_ELDEN/Elden-Ring-Wallpaper.jpg');">
+  <div id="footer" class="p-5">
+      <div class="container card card-border2 w-100 pt-5">
+        <div class="row row-cols-md-2 justify-content-md-center">
+          <div class="col text-center">
+            <h2>SUSCRÍBETE A NUESTRA NEWSLETTER</h2>
+          </div>
+        </div>
+        <div class="row justify-content-md-center py-5">
+          <div class="col text-center font-weight-light">
+            <p>¿Aún no te hemos BOMBARDEADO con información? Suscribete a nuestra newsletter y recibe todas las noticias antes que nadie sobre el mundo medieval</p>
+            <p><input type="text" name="email" style="width: 30%; padding: 5px 5px" placeholder="introduce tu correo. Ejemplo: jacinto@gmail.com"></p>
+            <p><button class="btn btn-lg rounded-pill btn-primary w-20  p-3 shadow-sm font-weight-bold">Let's go!</button></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+        <!--SEPARADOR-->
+        <section class="prueba" style="background-image: url('IMAGES_ELDEN/Elden-Ring-Wallpaper.jpg')">
+        <div id="separator">
+          <div class="content"></div>
+        </div>
+        </section>
+    <!--Footer-->
+    <div id="footer" class="p-5">
+      <div class="container w-50 pt-5">
+        <div class="row row-cols-md-2 justify-content-md-center">
+          <div class="col text-center">
+          </div>
+        </div>
+        <div class="row justify-content-md-center py-5">
+          <div class="col text-center font-weight-light">
+            <p>Es necesario disponer de conexión a internet para contactar con nuestro equipo. Para más información, escíbenos a <a href="mailto:info@medievalworld.es" class="mailto"><b>info@medievalworld.es</b></a></p>
+            <p>Todos los productos se venden por separado. Para más información contacta con el vendedor. Visita pccomponentes.es para detalles específicos del producto.</p>
+            <p>© 1997 - 2022 Medieval World. Medieval World properties are trademarks of Medieval World.</p>
+          </div>
+          
+        </div>
+        <div class="row"></div>
+        <a href="contacta.html">
+          <button class="btn btn-lg rounded-pill btn-primary w-100  p-3 shadow-sm font-weight-bold">¡Contáctanos!</button>
+        </a>
+      </div>
+    </div>
+  
+  
+
+</body>
+
+</html>
---
+++ resources/views/elden_productos.blade.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="https://kit.fontawesome.com/2a4935d0f4.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+        <!--NAV-->
+        <nav class="navbar navbar-expand-lg navbar-light" aria-label="Eleventh navbar example">
+          <div class="container-fluid">
+              <a class="navbar-brand mr-auto navhover2" href="home.html"><img src="img/logoMW.svg"></a>
+              <button class="navbar-toggler bg-light" 
+                      type="button" 
+                      data-bs-toggle="collapse" 
+                      data-bs-target="#navbarSupportedContent"
+                      aria-expanded="false"
+                      aria-label="Toogle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav ms-auto">
+                      <li class="nav-item navhover"><a class="nav-link" href="home_berserk.html"><img style="margin:15px" src="navimg/berserklogo.png" width="50px"></a></li>
+                      <li class="nav-item navhover"><a class="nav-link" href="home_esdla.html"><img style="margin:15px" src="navimg/esdlalogo.png" width="50px"></a></li>
+                      <li class="nav-item navhover"><a class="nav-link" href="home_eldenring.html"><img style="margin:15px" src="navimg/eldenringlogo.png" width="50px"></a></li>
+                  </ul>
+              </div>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                  <li class="nav-item navhover2"><a class="nav-link" href="contacta.html"><img src="navimg/contactlogo.svg" width="80px"></a></li>
+                  <li class="nav-item navhover2"><a class="nav-link" href="login.html"><img src="navimg/loginsignin_logo.svg" width="110px"></a></li>
+                  <li class="nav-item navhover2"><a class="nav-link" href="perfil.html"><img src="navimg/perfil_logo.svg" width="50px"></a></li>
+                </ul>
+              </div>
+          </div>
+      </nav>
+      <!--SEPARADOR-->
+<div id="separator">
+  <div class="content"></div>
+</div>
+    <!--NOTICIA-->
+    <article>
+        <section>
+            <div class="container-fluid py-2 text-center">
+                <h1 class="display-4">TOP 5 tiendas donde conseguir las claves para jugar a Elden Ring</h1></br>
+                <p class="text-muted">Te recomendamos el TOP 5 de las tiendas online donde conseguir Elden Ring para PC al mejor precio, ¡No te lo pierdas!</p></br>
+                <img src="IMAGES_ELDEN/BIG_ANIMATION2.GIF" class="w-50" alt="">
+                </br>
+            </div>
+            <div class="container py-2 text-center">
+                <div class="text-center  col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead" id="noticias">
+                  Pocas son las personas que, a día de hoy, no han disfrutado de algún Soulslike. Ya no hablamos de los creados por Hidetaka Papasote Miyazaki, sino obras independientes que han sido inspiradas por la fórmula del mismo (Jedi Fallen Order, Blasphemous, MyLittlePonny3...)
+                </div>
+                <p class="col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead">
+                  Desde el equipo de Medieval World recomendamos encarecidamente que, si es la primera vez que juegan a un juego de FromSoftware, comiencen por Elden Ring, pues es el más sencillo de abordar en primera instancia gracias a su mundo abierto.</p>
+                  <div class="text-center  col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead" id="noticias">
+                    En este post os dejamos las 5 mejores tiendas donde conseguir Elden Ring para PC:
+                  </div>
+                  <!--TIENDAS ONLINE-->
+                <div class="col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead">
+                <h1>NUESTRO TOP 5:</h1>
+                <br/>
+                </div>
+
+                <!--PRIMER ARTÍCULO-->
+                <div class="col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead">
+                <h3>1. KINGUIN. 40.03 €</h3>
+                <br/>
+                <img src="noticias/elden/c2.3.png" class="w-25" alt="">
+                <br/><br/>
+                <p><b>Tienda:</b> Kinguin</p>
+                <p><b>Precio:</b> 40.03 €</p>
+                <p><b>Garantía de pago:</b> Si</p>
+                <p><b>Si te interesa:</b> puedes encontrar el link a la oferta <a href="https://www.kinguin.net/category/111448/elden-ring-eu-steam-cd-key?currency=EUR&discount=AKS10&nosalesbooster=1&r=3445&utm_campaign=allkeyshop&utm_medium=referral&utm_source=www.allkeyshop.com">justo aquí.</a></p>
+                </div>
+              
+                <!--SEGUNDO ARTÍCULO-->
+                <div class="col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead">
+                  <h3>2. HRK. 42.97 €</h3>
+                  <br/>
+                  <img src="noticias/elden/c2.1.png" class="w-25" alt="">
+                  <br/><br/>
+                  <p><b>Nombre:</b> BERSERK MAXIMUM VOLUMEN 01</p>
+                  <p><b>Tienda:</b> HRK</p>
+                  <p><b>Precio:</b> 42.97 €</p>
+                  <p><b>Garantía de pago:</b> Si</p>
+                  <p><b>Si te interesa:</b> puedes encontrar el link a la oferta <a href="https://www.hrkgame.com/es/games/product/elden-ring-emea?utm_source=www.allkeyshop.com&utm_medium=referral&utm_campaign=allkeyshop">justo aquí.</a></p>
+                  </div>
+
+                  <!--TERCER ARTÍCULO-->
+                <div class="col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead">
+                  <h3>3. GAMIVO. 42.99 €</h3>
+                  <br/>
+                  <img src="noticias/elden/c2.2.png" class="w-25" alt="">
+                  <br/><br/>
+                  <p><b>Tienda:</b> GAMIVO</p>
+                  <p><b>Precio:</b> 42.99 €</p>
+                  <p><b>Garantía de pago:</b> Si</p>
+                  <p><b>Si te interesa:</b> puedes encontrar el link a la oferta <a href="https://www.gamivo.com/product/elden-ring-eu?glv=kiwhuamu&utm_source=www.allkeyshop.com&utm_medium=referral&utm_campaign=allkeyshop">justo aquí.</a></p>
+                  </div>
+
+                  <!--CUARTO ARTÍCULO-->
+                <div class="col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead">
+                  <h3>4. ENEBA. 43.59 €</h3>
+                  <br/>
+                  <img src="noticias/elden/c2.4.png" class="w-25" alt="">
+                  <br/><br/>
+                  <p><b>Tienda:</b> GAMIVO</p>
+                  <p><b>Precio:</b> 43.59 €</p>
+                  <p><b>Garantía de pago:</b> Si</p>
+                  <p><b>Si te interesa:</b> puedes encontrar el link a la oferta <a href="https://www.eneba.com/steam-elden-ring-pc-steam-key-europe-4?af_id=allkeyshop&utm_source=www.allkeyshop.com&utm_medium=referral&utm_campaign=allkeyshop">justo aquí.</a></p>
+                  </div>
+
+                  <!--QUINTO ARTÍCULO-->
+                <div class="col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead">
+                  <h3>5. G2A. 45.91 €</h3>
+                  <br/>
+                  <img src="noticias/elden/c2.5.png" class="w-25" alt="">
+                  <br/><br/>
+                  <p><b>Tienda:</b> G2A</p>
+                  <p><b>Precio:</b> 45.91 €</p>
+                  <p><b>Garantía de pago:</b> Si</p>
+                  <p><b>Si te interesa:</b> puedes encontrar el link a la oferta <a href="https://www.g2a.com/fr/elden-ring-pc-steam-key-europe-i10000273674006?utm_source=www.allkeyshop.com&utm_campaign=allkeyshop&utm_medium=referral&utm_content=COM_GLOBAL_PB_PLUS_GAM_LISTING_NOR_allkeyshopPLUS&aid=13148837&gname=dccb1b16c9">justo aquí.</a></p>
+                  </div>
+
+                  <div class="text-center  col-lg-12 col-md-12 col-sm-12 p-5 display-7 lead" id="noticias">
+                    NOTA: Recordad que como oviamente (no tan obvio) sucede con el flujo de merecado, los precios suelen variar en función de X cosas. Si quereis tener una lista actualizada, os recomiendo personalmente <a href="https://www.allkeyshop.com/blog/buy-elden-ring-cd-key-compare-prices/"> esta página de aqui</a>, donde podreis ver el mejor precio de las claves en directo
+                  </div>
+
+            </div>
+        </section>
+    </article>
+<!--PUBLICADO POR-->
+<div class="row align-items-center mb-4">
+  <div class="col-lg-3"></div>
+  <div class="col-md-6 col-lg-3 col-sm-6 text-center text-lg-left mb-3 m-lg-0">
+    <img
+      src="img/micareto.png"
+      alt="Avatar"
+      class="rounded shadow-1-strong mr-2"
+      height="35"/>
+    <span>Publicado <u>15.05.2022</u> por</span>
+    <a href="#" class="text-dark">José Sanchís AKA René Sanchís</a>
+  </div>
+  <div class="col-lg-3 col-md-6 col-sm-6 text-center text-lg-center">
+    <!-- Twitter -->
+    <a
+      class="btn btn-primary px-3 mr-1"
+      style="background-color: #55acee;"
+      href="https://twitter.com/bloodyReneTV"
+      role="button"
+      ><i class="fab fa-twitter"></i></a>
+  </div>
+</div>
+<!--SEPARADOR-->
+<div id="separator">
+  <div class="content"></div>
+</div>
+<!--NEWSLETTER-->
+<!--Footer-->
+<section class="prueba" style="background-image: url('IMAGES_ELDEN/Elden-Ring-Wallpaper.jpg');">
+  <div id="footer" class="p-5">
+      <div class="container card card-border2 w-100 pt-5">
+        <div class="row row-cols-md-2 justify-content-md-center">
+          <div class="col text-center">
+            <h2>SUSCRÍBETE A NUESTRA NEWSLETTER</h2>
+          </div>
+        </div>
+        <div class="row justify-content-md-center py-5">
+          <div class="col text-center font-weight-light">
+            <p>¿Aún no te hemos BOMBARDEADO con información? Suscribete a nuestra newsletter y recibe todas las noticias antes que nadie sobre el mundo medieval</p>
+            <p><input type="text" name="email" style="width: 30%; padding: 5px 5px" placeholder="introduce tu correo. Ejemplo: jacinto@gmail.com"></p>
+            <p><button class="btn btn-lg rounded-pill btn-primary w-20  p-3 shadow-sm font-weight-bold">Let's go!</button></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+        <!--SEPARADOR-->
+        <section class="prueba" style="background-image: url('IMAGES_ELDEN/Elden-Ring-Wallpaper.jpg')">
+        <div id="separator">
+          <div class="content"></div>
+        </div>
+        </section>
+    <!--Footer-->
+    <div id="footer" class="p-5">
+      <div class="container w-50 pt-5">
+        <div class="row row-cols-md-2 justify-content-md-center">
+          <div class="col text-center">
+          </div>
+        </div>
+        <div class="row justify-content-md-center py-5">
+          <div class="col text-center font-weight-light">
+            <p>Es necesario disponer de conexión a internet para contactar con nuestro equipo. Para más información, escíbenos a <a href="mailto:info@medievalworld.es" class="mailto"><b>info@medievalworld.es</b></a></p>
+            <p>Todos los productos se venden por separado. Para más información contacta con el vendedor. Visita pccomponentes.es para detalles específicos del producto.</p>
+            <p>© 1997 - 2022 Medieval World. Medieval World properties are trademarks of Medieval World.</p>
+          </div>
+          
+        </div>
+        <div class="row"></div>
+        <a href="contacta.html">
+          <button class="btn btn-lg rounded-pill btn-primary w-100  p-3 shadow-sm font-weight-bold">¡Contáctanos!</button>
+        </a>
+      </div>
+    </div>
+  
+  
+
+</body>
+
+</html>
